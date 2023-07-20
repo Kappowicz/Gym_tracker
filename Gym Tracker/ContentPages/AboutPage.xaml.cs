b5ba82369b001d445c35b65f14c5737e87a97218
Kappowicz <113104296+Kappowicz@users.noreply.github.com>
2023-07-20 22:01:09 +0200

    improve AboutPage

--- Gym Tracker/ContentPages/AboutPage.xaml.cs
+++ Gym Tracker/ContentPages/AboutPage.xaml.cs
@@ -2,33 +2,59 @@ namespace Gym_Tracker;
 
 public sealed partial class AboutPage : ContentPage
 {
+    //Icons from flaticon has to have their own button according to flaticon's license,
+    //Livecharts doesn't have to but this is cool so I'll leave it there
+
+    private const string GithubUrl = "https://github.com/Kappowicz/Gym_tracker";
+    private const string ProgressChartsUrl = "https://lvcharts.com/";
+    private const string StadisticsIconUrl = "https://www.flaticon.com/free-icons/stadistics";
+    private const string WorkoutIconUrl = "https://www.flaticon.com/free-icons/workout";
+    private const string UpArrowIconUrl = "https://www.flaticon.com/free-icons/up-arrow";
+
     public AboutPage()
     {
         InitializeComponent();
     }
 
+    private async Task OpenUrlAsync(string url)
+    {
+        try
+        {
+            await Launcher.OpenAsync(url);
+        }
+        catch (Exception ex)
+        {
+            await DisplayPopUpErrorURLMessage(ex.Message);
+        }
+    }
+
+    public async Task DisplayPopUpErrorURLMessage(string message)
+    {
+        await DisplayAlert("URL Error", $"Error occurred while opening this URL: {message}.", "OK");
+    }
+
     private async void OpenGithubLinkButtonClicked(object sender, EventArgs e)
     {
-        _ = await Launcher.Default.OpenAsync("https://github.com/Kappowicz/Gym_tracker");
+        await OpenUrlAsync(GithubUrl);
     }
 
     private async void ProgressChartsButtonClicked(object sender, EventArgs e)
     {
-        _ = await Launcher.Default.OpenAsync("https://lvcharts.com/"); //That's cool stuff, I'll leave it there even if i don't have to
+        await OpenUrlAsync(ProgressChartsUrl);
     }
 
     private async void StadisticsIconButtonClicked(object sender, EventArgs e)
     {
-        _ = await Launcher.Default.OpenAsync("https://www.flaticon.com/free-icons/stadistics");
+        await OpenUrlAsync(StadisticsIconUrl);
     }
 
     private async void WorkoutIconButtonClicked(object sender, EventArgs e)
     {
-        _ = await Launcher.Default.OpenAsync("https://www.flaticon.com/free-icons/workout");
+        await OpenUrlAsync(WorkoutIconUrl);
     }
 
-    private async void UpDownArrowButtonClicked(object sender, EventArgs e)
+    private async void UpArrowButtonClicked(object sender, EventArgs e)
     {
-        _ = await Launcher.Default.OpenAsync("https://www.flaticon.com/free-icons/up-arrow");
+        await OpenUrlAsync(UpArrowIconUrl);
     }
 }
